3fa7f6f8a529d5d6467549edfe7d2808eaefb6f1
francis <alfrancis302@gmail.com>
2024-08-15 20:58:05 +0800

    Organize files

--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,5 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../components/HomeView.vue'
+import Home from '@/views/SimmerWorks.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -7,7 +7,7 @@ const router = createRouter({
     {
       path: '/',
       name: 'home',
-      component: HomeView
+      component: Home
     },
     {
       path: '/works',
--- src/views/SimmerWorks.vue
+++ src/views/SimmerWorks.vue
@@ -1,45 +1,19 @@
 <template>
-  <MainNav />
-  <main class="main">
-    <RouterView />
-    <LastSection />
-    <FooterSection />
-  </main>
+    <HeroSection />
+    <PreparationSection />
+    <AboutUs />
 </template>
 
 <script>
-import MainNav from "@/components/MainNav.vue";
-import LastSection from "@/components/LastSection.vue";
-import FooterSection from "@/components/FooterSection.vue";
+import HeroSection from "@/components/Home/HeroSection.vue";
+import PreparationSection from "@/components/Home/PreparationSection.vue";
+import AboutUs from "@/components/Home/AboutUs.vue";
 
-export default {
-  components: {
-    MainNav,
-    LastSection,
-    FooterSection,
-  },
-};
+    export default {
+        components: {
+            HeroSection,
+            PreparationSection,
+            AboutUs,
+        }
+    }
 </script>
-<style lang="scss">
-:root {
-  --black: #000000;
-  --white-400: #f7ede1;
-  --orange-600: #fa9a11;
-  --red-600: #bf3110;
-  --green-600: #155942;
-  --blue: #007bff;
-}
-
-html, 
-html:focus-within {
-  scroll-behavior: auto;
-}
-
-.main {
-  margin-top: 5.6rem;
-}
-
-.container {
-  max-width: 1400px;
-}
-</style>
